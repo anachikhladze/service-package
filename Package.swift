f92f3e295630e5cd56343a91bd9e346282fdb8da
Anna Sumire <ana@Anas-MacBook-Air.local>
2023-11-19 22:54:18 +0400

    third commit

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FactService",
-            dependencies: [], path: "Sources/FactService"),
+            dependencies: [], path: "Sources"),
         .testTarget(
             name: "FactServiceTests",
             dependencies: ["FactService"]),
--- Sources/FactService/FactService.swift
+++ Sources/FactService/FactService.swift
@@ -1,9 +1,45 @@
-public struct FactService {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public final class FactService {
+    struct FactModel: Decodable {
+        let data: [Fact]
     }
-
-   
-
+    
+    struct Fact: Decodable {
+        let fact: String
+    }
+    
+    
+    static let shared = FactService ()
+    
+    public init() {}
+    
+    func fetchFacts(completion: @escaping (Result<[Fact], Error>) -> Void) {
+        let urlStr = "https://catfact.ninja/facts?limit=20"
+        
+        
+        guard let url = URL(string: urlStr) else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure (error))
+                return
+            }
+            
+            guard let data = data else {
+                completion (. failure (NSError (domain: "", code: -2, userInfo: [NSLocalizedDescriptionKey: "No data received" ])))
+                return
+            }
+            
+            do {
+                let factResponse = try JSONDecoder().decode(FactModel.self, from: data)
+                completion(.success (factResponse.data))
+            } catch {
+                completion(.failure (error))
+            }
+        }.resume ()
+    }
 }
